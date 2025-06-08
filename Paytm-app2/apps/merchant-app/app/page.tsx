7768f66231cae56efc0fb92b06ea31b1b48b231a
Aksh002 <akshitgangwar02@gmail.com>
2025-06-09 02:57:00 +0530

    scheduled push of 9/6/25

--- Paytm-app2/apps/merchant-app/app/page.tsx
+++ Paytm-app2/apps/merchant-app/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Card } from "@repo/ui/card";
 import { Gradient } from "@repo/ui/gradient";
 import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+import { useBalance } from "@repo/store/useBalance"
 
 const LINKS = [
   {
