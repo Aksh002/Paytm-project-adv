a546d1c05b912564f16ea5dca81578133f67286a
Aksh002 <akshitgangwar02@gmail.com>
2025-06-08 19:08:18 +0530

    scheduled push of 8/6/25v2

--- Paytm-app2/apps/web/app/page.tsx
+++ Paytm-app2/apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Card } from "@repo/ui/card";
 import { Gradient } from "@repo/ui/gradient";
 import { TurborepoLogo } from "@repo/ui/turborepo-logo";
 import { Test } from "@repo/ui/components/Test";
-
+import { PrismaClient } from "@repo/db/client"
 
 const LINKS = [
   {
